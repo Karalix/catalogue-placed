f38def1ca2935dc4a09860332bb71943c55a8a53
Ducros Alix <alix@kronikle.eu>
2022-05-09 17:42:19 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,21 +3,23 @@
     <div class="header">
       <img class="placed-logo" src="~/assets/Logo_Placed.svg"/>
     </div>
-    <div class="main">
-      <h1>PLACED</h1>
-      <p class="intro">How can new and dynamic digital services help libraries transform into vibrant urban innovation hubs by supporting knowledge creation activities among citizens and turning this knowledge into an ever-growing part of the urban community's shared resources?</p>
-      <p>The PLACED project introduces a new type of place- and activity-centric digital library services. Whereas library services typically focus on providing access to a collection of media, PLACED services support activities in the library.</p>
-      <p>The groundbreaking aspect is that these services capture knowledge generated through these activities, make them a part of the library's collection, and allow future library users to explore and access them. In this way, PLACED services help break down the institutional walls of the library and make it an integrated part of urban life by creating an ever-evolving collection built on urban activities and knowledge generation.</p>
-      <br/>
-      <h2>Learn more about PLACED</h2>
-      <ul>
-        <li>The website of the project<a href="https://placedproject.eu/">placedproject.eu</a></li>
-        <li>Our current work bringing our prototypes to the field <a href="https://www.kronikle.eu">kronikle.eu</a></li>
-      </ul>
-      <p>Find below the links to the 2 catalogues summarizing our results, in english and in french :</p>
-      <div class="links">
-        <a href="/fr-catalogue-s.pdf">Version Française</a>
-        <a href="/eng-catalogue-v3-s.pdf">English Version</a>
+    <div class="ubermain">
+      <div class="main">
+        <h1>PLACED</h1>
+        <p class="intro">How can new and dynamic digital services help libraries transform into vibrant urban innovation hubs by supporting knowledge creation activities among citizens and turning this knowledge into an ever-growing part of the urban community's shared resources?</p>
+        <p>The PLACED project introduces a new type of place- and activity-centric digital library services. Whereas library services typically focus on providing access to a collection of media, PLACED services support activities in the library.</p>
+        <p>The groundbreaking aspect is that these services capture knowledge generated through these activities, make them a part of the library's collection, and allow future library users to explore and access them. In this way, PLACED services help break down the institutional walls of the library and make it an integrated part of urban life by creating an ever-evolving collection built on urban activities and knowledge generation.</p>
+        <br/>
+        <h2>Learn more about PLACED</h2>
+        <ul>
+          <li>The website of the project <a href="https://placedproject.eu/">placedproject.eu</a></li>
+          <li>Our current work bringing our prototypes to the field <a href="https://www.kronikle.eu">kronikle.eu</a></li>
+        </ul>
+        <p>Find below the links to the 2 catalogues summarizing our results, in english and in french :</p>
+        <div class="links">
+          <a href="/fr-catalogue-s.pdf">Version Française</a>
+          <a href="/eng-catalogue-v3-s.pdf">English Version</a>
+        </div>
       </div>
     </div>
   </div>
@@ -42,12 +44,16 @@ html {
   width: 120px
 }
 
+.ubermain {
+  display: flex;
+  align-items: center;
+}
+
 .main {
   display: flex;
   flex-direction: column;
   justify-content: center;
   justify-items: center;
-  align-items: center;
   padding: 0.5em;
 }
 
